d8b238b6e688b82a506f58624df1d454f7da3187
Trubin <ATrubin@kiev.luxoft.com>
2022-06-14 16:04:37 +0300

    Orders page

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import DateChooserCard from "./components/DateChooserCard/DateChooserCard";
+import OrdersList from "./components/OrdersList/OrdersList";
+import BasicTabs from "./components/BasicTabs/BasicTabs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Box sx={{flexGrow: 1}}>
+                    <Grid container spacing={15}>
+                        <Grid item xs={2}>
+                            <DateChooserCard/>
+                        </Grid>
+                        <Grid item xs={10}>
+                            <BasicTabs/>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
