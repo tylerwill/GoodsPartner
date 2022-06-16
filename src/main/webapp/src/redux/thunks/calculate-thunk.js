abea267304bffb90dcc864d488728cdca387d45b
TarasOrlovskyi <taras.orlovskyi@gmail.com>
2022-06-16 15:42:41 +0300

    in calculate-thunk deleted 'debugger'

--- src/main/webapp/src/redux/thunks/calculate-thunk.js
+++ src/main/webapp/src/redux/thunks/calculate-thunk.js
@@ -3,7 +3,6 @@ import {calculateAllDataByDate} from "../actions/calculate-action";
 
 export const getCalculatedDataByDate = (ordersDate) => async dispatch => {
   let calculatedData = await calculateApi.calculateByDateRequest(ordersDate);
-  debugger
   // dispatch(calculateAllDataByDate(calculatedData));
   dispatch(calculateAllDataByDate(calculatedData.data));
 }
