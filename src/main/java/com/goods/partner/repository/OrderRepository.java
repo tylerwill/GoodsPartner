600879dd539664f7f82f060578c16d0bfb68f52e
BogdanParashchak <bogdan1986@ukr.net>
2022-06-16 19:56:24 +0300

    Refactor for creation and shipping dates handling

--- src/main/java/com/goods/partner/repository/OrderRepository.java
+++ src/main/java/com/goods/partner/repository/OrderRepository.java
@@ -12,6 +12,5 @@ public interface OrderRepository extends JpaRepository<Order, LocalDate> {
 
     // TODO require join query to avoid data enrichment
 //    @Query("SELECT o FROM Order WHERE o.date = ?1")
-    List<Order> findAllByDateBefore(LocalDate date);
-
+    List<Order> findAllByShippingDateBefore(LocalDate date);
 }
