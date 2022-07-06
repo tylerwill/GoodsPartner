1753169e7b3d88c132f90f7433e5c921460146ab
0ffffh <0f7f7f7@gmail.com>
2022-07-07 02:15:09 +0300

    refactored StoreMapperTest: -removed static fileads -add @TestInstance(PER_CLASS) annotation -swap expected and actual values -add test case for lists size > 1

--- src/test/java/com/goods/partner/mapper/StoreMapperTest.java
+++ src/test/java/com/goods/partner/mapper/StoreMapperTest.java
@@ -6,21 +6,24 @@ import com.goods.partner.entity.projection.StoreProjection;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.mockito.Mockito.*;
 
-
+@TestInstance(PER_CLASS)
 class StoreMapperTest {
 
-    private final StoreMapper storeMapper = new StoreMapper();
-    private static StoreProjection storeProjection;
+    private StoreMapper storeMapper = new StoreMapper();
+    private StoreProjection storeProjection;
 
     @BeforeAll
-    static void setup() {
-        storeProjection = new StoreProjection(1, "Склад №1", 1, 5, 50.0);
+    void setup() {
+        storeMapper = new StoreMapper();
+        storeProjection = new StoreProjection(1, "Склад №1", 1, 5, 50.5);
     }
 
 
@@ -30,46 +33,47 @@ class StoreMapperTest {
 
         StoreDto storeDto = storeMapper.getStoreDto(storeProjection);
 
-        assertEquals(storeDto.getStoreId(), storeProjection.getStoreId());
-        assertEquals(storeDto.getStoreName(), storeProjection.getStoreName());
+        assertEquals(1, storeDto.getStoreId());
+        assertEquals("Склад №1", storeDto.getStoreName());
 
     }
 
     @Test
     @DisplayName("Get StoreDtoList from StoreProjection List")
     void test_givenStoreProjectionList_whenGetStoreDto_thenReturnStoreDtoList() {
+
         List<StoreDto> storeDtoList = storeMapper.mapStoreGroup(List.of(storeProjection));
 
-        assertEquals(storeDtoList.size(), 1);
+        assertEquals(1, storeDtoList.size());
 
         StoreDto storeDto = storeDtoList.get(0);
 
-        assertEquals(storeDto.getStoreId(), storeProjection.getStoreId());
-        assertEquals(storeDto.getStoreName(), storeProjection.getStoreName());
+        assertEquals(1, storeProjection.getStoreId());
+        assertEquals("Склад №1", storeProjection.getStoreName());
 
 
         List<StoreOrderDto> orders = storeDto.getOrders();
-        assertEquals(orders.size(), 1);
+        assertEquals(1, orders.size());
 
         StoreOrderDto storeOrderDto = orders.get(0);
 
-        assertEquals(storeOrderDto.getOrderId(), storeProjection.getOrderId());
-        assertEquals(storeOrderDto.getOrderNumber(), storeProjection.getOrderNumber());
-        assertEquals(storeOrderDto.getTotalOrderWeight(), storeProjection.getTotalOrderWeight());
+        assertEquals(1, storeOrderDto.getOrderId());
+        assertEquals(5, storeOrderDto.getOrderNumber());
+        assertEquals(50.5, storeOrderDto.getTotalOrderWeight(), 0.001);
 
     }
 
     @Test
     @DisplayName("Get StoreDtoList from StoreProjection List checking method calls")
     void test_givenStoreProjectionList_whenGetStoreDto_thenReturnStoreDtoListVerify() {
-
+        StoreProjection anotherStoreProjection = new StoreProjection(2, "Склад №2", 1, 5, 50.5);
         StoreMapper spyStoreMapper = spy(storeMapper);
+        List<StoreDto> storeDtoList = spyStoreMapper
+                .mapStoreGroup(List.of(storeProjection, anotherStoreProjection));
 
-        List<StoreDto> storeDtoList = spyStoreMapper.mapStoreGroup(List.of(storeProjection));
-
-        assertEquals(storeDtoList.size(), 1);
-
-        verify(spyStoreMapper, times(1)).getStoreDto(storeProjection);
+        assertEquals(2, storeDtoList.size());
+        verify(spyStoreMapper).mapStoreGroup(anyList());
+        verify(spyStoreMapper, times(2)).getStoreDto(any(StoreProjection.class));
     }
 
 
