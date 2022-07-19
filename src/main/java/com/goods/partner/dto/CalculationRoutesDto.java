8b7eb9d3c4874a1aab6861726c19dcca7a79ae73
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-07-19 09:29:25 +0300

    refactored CalculationRoutesDto:
    - add @Builder;
    - add @AllArgsConstructor, @NoArgsConstructor;
    - delete empty line.

--- src/main/java/com/goods/partner/dto/CalculationRoutesDto.java
+++ src/main/java/com/goods/partner/dto/CalculationRoutesDto.java
@@ -1,13 +1,15 @@
 package com.goods.partner.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CalculationRoutesDto {
     private LocalDate date;
     private List<RouteDto> routes;
