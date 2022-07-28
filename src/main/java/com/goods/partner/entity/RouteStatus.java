d234c0b7513bae18eeea1c361950205fa65359aa
Ishei5 <pankovisheev5@gmail.com>
2022-07-28 22:35:46 +0300

    Simplified enum RouteStatus

--- src/main/java/com/goods/partner/entity/RouteStatus.java
+++ src/main/java/com/goods/partner/entity/RouteStatus.java
@@ -1,16 +1,9 @@
 package com.goods.partner.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
 public enum RouteStatus {
-    DRAFT("DRAFT"),
-    APPROVED("APPROVED"),
-    INPROGRESS("INPROGRESS"),
-    COMPLETED("COMPLETED"),
-    INCOMPLETE("INCOMPLETE");
-
-    @Getter
-    private final String value;
+    DRAFT,
+    APPROVED,
+    INPROGRESS,
+    COMPLETED,
+    INCOMPLETE;
 }
