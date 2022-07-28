80d8e1d4851403cf4e4e899fb1da547ea1f1e08e
Ishei5 <pankovisheev5@gmail.com>
2022-07-28 23:45:15 +0300

    Add string field 'status'

--- src/main/java/com/goods/partner/entity/RouteStatus.java
+++ src/main/java/com/goods/partner/entity/RouteStatus.java
@@ -1,9 +1,16 @@
 package com.goods.partner.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public enum RouteStatus {
-    DRAFT,
-    APPROVED,
-    INPROGRESS,
-    COMPLETED,
-    INCOMPLETE;
+    DRAFT("DRAFT"),
+    APPROVED("APPROVED"),
+    INPROGRESS("INPROGRESS"),
+    COMPLETED("COMPLETED"),
+    INCOMPLETE("INCOMPLETE");
+
+    @Getter
+    private final String status;
 }
