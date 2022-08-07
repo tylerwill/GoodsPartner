0b523e87a0e7e60124fb611d8f55e060c5801d70
Tolik <trubintolik@gmail.com>
2022-08-07 21:27:05 +0200

    [refactoring] Fix test configuration

--- goods-partner-backend/src/test/java/com/goodspartner/AbstractBaseITest.java
+++ goods-partner-backend/src/test/java/com/goodspartner/AbstractBaseITest.java
@@ -8,26 +8,26 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-//@Testcontainers
+@Testcontainers
 @ActiveProfiles("test")
 public class AbstractBaseITest {
 
-//    private static final PostgreSQLContainer<?> POSTGRES_SQL_CONTAINER;
-//
-//    static {
-//        POSTGRES_SQL_CONTAINER =
-//                new PostgreSQLContainer<>("postgres:14.4")
-//                        .withDatabaseName("test")
-//                        .withUsername("test")
-//                        .withPassword("test")
-//                        .withReuse(true);
-//        POSTGRES_SQL_CONTAINER.start();
-//    }
-//
-//    @DynamicPropertySource
-//    public static void overrideProps(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", POSTGRES_SQL_CONTAINER::getJdbcUrl);
-//        registry.add("spring.datasource.username", POSTGRES_SQL_CONTAINER::getUsername);
-//        registry.add("spring.datasource.password", POSTGRES_SQL_CONTAINER::getPassword);
-//    }
+    private static final PostgreSQLContainer<?> POSTGRES_SQL_CONTAINER;
+
+    static {
+        POSTGRES_SQL_CONTAINER =
+                new PostgreSQLContainer<>("postgres:14.4")
+                        .withDatabaseName("test")
+                        .withUsername("test")
+                        .withPassword("test")
+                        .withReuse(true);
+        POSTGRES_SQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_SQL_CONTAINER::getPassword);
+    }
 }
