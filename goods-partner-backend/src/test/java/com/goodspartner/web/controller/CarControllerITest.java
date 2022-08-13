93d90cf88718ab286cb690f136cacbc19c7caad3
Tolik <trubintolik@gmail.com>
2022-08-14 00:56:36 +0200

    remove unnecessary transactional from tests

--- goods-partner-backend/src/test/java/com/goodspartner/web/controller/CarControllerITest.java
+++ goods-partner-backend/src/test/java/com/goodspartner/web/controller/CarControllerITest.java
@@ -19,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DBRider
-@Transactional
 public class CarControllerITest extends AbstractWebITest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
