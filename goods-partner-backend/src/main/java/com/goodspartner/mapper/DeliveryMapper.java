d20f55785e38c0f46fbe39bfdf763d0eccf96f90
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-09-29 09:34:15 +0300

    refactor DeliveryControllerITest with deliveryShortDto

--- goods-partner-backend/src/main/java/com/goodspartner/mapper/DeliveryMapper.java
+++ goods-partner-backend/src/main/java/com/goodspartner/mapper/DeliveryMapper.java
@@ -1,10 +1,12 @@
 package com.goodspartner.mapper;
 
 import com.goodspartner.dto.DeliveryDto;
+import com.goodspartner.dto.DeliveryShortDto;
 import com.goodspartner.entity.Delivery;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 import java.util.List;
 
@@ -28,4 +30,15 @@ public interface DeliveryMapper {
     Delivery deliveryDtoToDelivery(DeliveryDto deliveryDto);
 
     List<DeliveryDto> deliveriesToDeliveryDtos(List<Delivery> deliveries);
+
+    @Named("deliveryToDeliveryShortDto")
+    default DeliveryShortDto deliveryToDeliveryShortDto(Delivery delivery) {
+        return DeliveryShortDto.builder()
+                .id(delivery.getId())
+                .deliveryDate(delivery.getDeliveryDate())
+                .status(delivery.getStatus())
+                .routeCount(delivery.getRoutes().size())
+                .orderCount(delivery.getOrders().size())
+                .build();
+    }
 }
--- goods-partner-backend/src/test/java/com/goodspartner/web/controller/DeliveryControllerITest.java
+++ goods-partner-backend/src/test/java/com/goodspartner/web/controller/DeliveryControllerITest.java
@@ -124,7 +124,9 @@ class DeliveryControllerITest extends AbstractWebITest {
 
         mockMvc.perform(get("/api/v1/deliveries/123e4567-e89b-12d3-a456-556642440000")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content()
+                .json(getResponseAsString("response/delivery-controller.json")));
     }
 
     @Test
@@ -134,7 +136,9 @@ class DeliveryControllerITest extends AbstractWebITest {
 
         mockMvc.perform(get("/api/v1/deliveries")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content()
+                .json(getResponseAsString("response/delivery-controller-with-delivery-short-dto.json")));
     }
 
     @Test
