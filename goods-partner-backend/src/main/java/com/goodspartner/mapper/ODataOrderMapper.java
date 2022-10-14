c29c7ca75e616d134b61cc914a3a57d4188020a0
mbiloshytskyi <belosh59@gmail.com>
2022-10-14 01:19:24 +0100

    populate timewindow with default values

--- goods-partner-backend/src/main/java/com/goodspartner/mapper/ODataOrderMapper.java
+++ goods-partner-backend/src/main/java/com/goodspartner/mapper/ODataOrderMapper.java
@@ -3,12 +3,17 @@ package com.goodspartner.mapper;
 import com.goodspartner.dto.OrderDto;
 import com.goodspartner.service.dto.external.grandedolce.ODataOrderDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.time.LocalTime;
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = {LocalTime.class})
 public interface ODataOrderMapper {
 
+    // TODO move to properties and more graceful enrichment
+    @Mapping(target = "deliveryStart", expression = "java(LocalTime.of(9, 0))")
+    @Mapping(target = "deliveryFinish", expression = "java(LocalTime.of(18, 0))")
     OrderDto toOrderDto(ODataOrderDto oDataOrderDto);
 
     List<OrderDto> toOrderDtosList(List<ODataOrderDto> oDataOrderDtos);
